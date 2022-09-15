9c36785fdfbb483f1d35c0c117dd1d9a98116f2e
khoerulmutaqinkm <92683033+khoerulmutaqinkm@users.noreply.github.com>
2022-09-15 19:53:15 +0700

    Update ContactTypesController.cs

--- AWork.Web/Controllers/Person/ContactTypesController.cs
+++ AWork.Web/Controllers/Person/ContactTypesController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering; 
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using AWork.Domain.Models;
 using AWork.Persistence;
