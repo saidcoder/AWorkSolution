1003247f736e021b96250af2dd6689b71df467bb
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-09-15 13:08:40 +0700

    Update EmailAddres And ContactType

--- AWork.Web/Startup.cs
+++ AWork.Web/Startup.cs
@@ -1,6 +1,8 @@
+using AWork.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace AWork.Web
 {
@@ -24,6 +27,11 @@ namespace AWork.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //register dbcontext
+            services.AddDbContext<AdventureWorks2019Context>(opts =>
+            {
+                opts.UseSqlServer(Configuration["ConnectionStrings:AdventureWorks2019"]);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
