9c7537a0ee102c6c45ff9631a72e511d5b8133b7
khoerulmutaqinkm <khoerulmutaqin225@gmail.com>
2022-09-20 16:22:08 +0700

    Update Person

---
+++ AWork.Domain/Repositories/Person/IPasswordRepository.cs
@@ -0,0 +1,23 @@
+﻿using AWork.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWork.Domain.Repositories.Person
+{
+    public interface IPasswordRepository
+    {
+        Task<IEnumerable<Password>> GetAllPassword(bool trackChanges);
+
+        Task<Password> GetPasswordById(int passwordId, bool trackChanges);
+
+        void Insert(Password password);
+
+        void Edit(Password password);
+
+        void Remove(Password password);
+        /*        public virtual Person BusinessEntity { get; set; }*/
+    }
+}
